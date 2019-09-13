85d29bdae3783f5f4fefba18a7dfa7e9e02aad66
Miao Fang from MateBook <1816232121@qq.com>
2019-09-13 14:41:21 +0800

    190913

--- src/DesignPatterns/SingletonDemo.java
+++ src/DesignPatterns/SingletonDemo.java
@@ -3,9 +3,10 @@ package DesignPatterns;
 /**
  * 二次检查锁定
  * 比直接用synchronized效率更高
+ * DoubleCheckLock
  */
 public class SingletonDemo {
-    private static volatile SingletonDemo instance = null;
+    private static volatile SingletonDemo instance = null;//添加volatile！！
 
     private SingletonDemo() {}
 
@@ -13,7 +14,8 @@ public class SingletonDemo {
         if(instance == null) {
             synchronized (SingletonDemo.class) {
                 if(instance == null) {
-                    instance = new SingletonDemo();
+                    instance = new SingletonDemo();//添加了volatile，保证对象初始化时不被优化指令重排序
+                    //如果没有加volatile，可能被指令重排，这会使得初始化一半/未完成的instance被return，给外界使用，程序会崩溃
                 }
             }
         }
