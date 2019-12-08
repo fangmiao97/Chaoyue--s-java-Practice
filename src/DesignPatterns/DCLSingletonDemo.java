dfead1a289a3cce0819844b1b13f19df763d7924
Miao Fang from MateBook <1816232121@qq.com>
2019-12-08 12:38:53 +0800

    191208 add IoDH singleton

--- src/DesignPatterns/DCLSingletonDemo.java
+++ src/DesignPatterns/DCLSingletonDemo.java
@@ -4,17 +4,18 @@ package DesignPatterns;
  * 二次检查锁定
  * 比直接用synchronized效率更高
  * DoubleCheckLock
+ *
  */
-public class SingletonDemo {
-    private static volatile SingletonDemo instance = null;//添加volatile！！
+public class DCLSingletonDemo {
+    private static volatile DCLSingletonDemo instance = null;//添加volatile！！
 
-    private SingletonDemo() {}
+    private DCLSingletonDemo() {}
 
-    public static SingletonDemo getInstance() {
+    public static DCLSingletonDemo getInstance() {
         if(instance == null) {
-            synchronized (SingletonDemo.class) {
+            synchronized (DCLSingletonDemo.class) {
                 if(instance == null) {
-                    instance = new SingletonDemo();//添加了volatile，保证对象初始化时不被优化指令重排序
+                    instance = new DCLSingletonDemo();//添加了volatile，保证对象初始化时不被优化指令重排序
                     //如果没有加volatile，可能被指令重排，这会使得初始化一半/未完成的instance被return，给外界使用，程序会崩溃
                 }
             }
---
+++ src/DesignPatterns/IoDHSingletonLoadBalancer.java
@@ -0,0 +1,70 @@
+package DesignPatterns;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * initialization-on demand
+ * 实现一个负载均衡器Load Balancer
+ * 当一个类被jvm加载的时候，会经历初始化的过程。
+ *
+ *
+ */
+public class IoDHSingletonLoadBalancer {
+    //可用服务器集合
+    private List serverList = null;
+
+    //私有构造方法
+    private IoDHSingletonLoadBalancer() {
+        serverList = new ArrayList();
+    }
+
+    //静态类，直到jvm确定其会被使用，才会初始化
+    //也就是在执行getInstance的时候，才会初始化LazyHolder，并初始化静态变量instance
+    private static class LazyHolder {
+        static final IoDHSingletonLoadBalancer instance = new IoDHSingletonLoadBalancer();
+    }
+
+    public static IoDHSingletonLoadBalancer getInstance() {
+        return LazyHolder.instance;
+    }
+
+    //增加服务器
+    public void addServer(String server) {
+        serverList.add(server);
+    }
+
+    //删除
+    public void removeServer(String server) {
+        serverList.remove(server);
+    }
+
+    //分配服务器（随机）
+    public String getServer() {
+        Random random = new Random();
+        int i = random.nextInt(serverList.size());
+        return (String) serverList.get(i);
+    }
+
+    public static void main(String[] args) {
+        IoDHSingletonLoadBalancer LB1, LB2, LB3;
+        LB1 = IoDHSingletonLoadBalancer.getInstance();
+        LB2 = IoDHSingletonLoadBalancer.getInstance();
+        LB3 = IoDHSingletonLoadBalancer.getInstance();
+
+        if(LB1 == LB2 && LB2 == LB3 && LB1 == LB3) {
+            System.out.println("same instance");
+        }
+
+        LB1.addServer("server 1");
+        LB1.addServer("server 2");
+        LB1.addServer("server 3");
+        LB1.addServer("server 4");
+
+        for (int i = 0; i < 10; i++) {
+            String server = LB1.getServer();
+            System.out.println("get server" + server);
+        }
+    }
+}
